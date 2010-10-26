17e59de7590fa94f6974d4f51df4c0278ffb78ff
Yuvi Masory <ymasory@gmail.com>
2010-10-26 13:53:32 -0400

    correct accidental newline

--- config.py
+++ config.py
@@ -44,8 +44,7 @@ MIN_SPACING=2 	        # min # of items between study and test.
 
 USE_INTERFERENCE=0	# use interference when constructing lists?
 
-USE
-_CORRELATION=1	# use correlations when constructing pairs?
+USE_CORRELATION=1	# use correlations when constructing pairs?
 
 MIN_CORRELATION=-.1
 
