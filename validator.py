9a46a9a42e23b16a25e547557adfea5529181eb2
Yuvi Masory <ymasory@gmail.com>
2010-10-26 14:26:47 -0400

    add shell

--- validator.py
+++ validator.py
@@ -11,15 +11,29 @@ def validate():
     subjects = map(lambda s: os.getcwd() + os.sep + 'data' + os.sep + s, os.listdir('data'))
     map(lambda s: validate_subject(s), subjects)
 
+
 def validate_subject(path):
     print "validating: " + path
     sesslog = open(path + os.sep + 'session.log', 'r').readlines()
     sesslog = map(lambda s: s.rstrip(), sesslog)
     sesslog = map(lambda s: s.split('\t'), sesslog)
-    print sesslog[0]
-    
-    
+    assert sesslog[0][-1] == 'Logging Begins'
+    assert sesslog[-1][-1] == 'Logging Ends'
 
+    validate_study(path)
+    validate_recall(path)
+    
+    print path + ' is valid'
+    print
+
+
+def validate_study(path):
+    pass
+
+
+def validate_recall(path):
+    pass
+    
 
 if __name__ == '__main__':
     validate()
